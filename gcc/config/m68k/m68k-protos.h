217eb961c3b58bffb12c3d74cc0e86be72ff678c
Fredrik Olsson <peylow@apple.com>
2013-12-05 12:38:16 +0100

    Moved scheduling code into its own file, and make omit frame pointer default when optimizing.

--- gcc/config/m68k/m68k-protos.h
+++ gcc/config/m68k/m68k-protos.h
@@ -77,6 +77,8 @@ extern rtx m68k_unwrap_symbol (rtx, bool);
 extern enum attr_cpu m68k_sched_cpu;
 extern enum attr_mac m68k_sched_mac;
 
+extern void m68k_setup_sched_options ();
+
 extern enum attr_opx_type m68k_sched_attr_opx_type (rtx, int);
 extern enum attr_opy_type m68k_sched_attr_opy_type (rtx, int);
 extern enum attr_size m68k_sched_attr_size (rtx);
